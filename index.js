9ff13d10f235580ff836468e3523fbd8931ec99d
napple29 <snwxxx29@gmail.com>
2021-11-29 15:30:28 +0900

    undifinedが返るため、オブジェクト内の数値を変更

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const outputAffiliationCatchCopy = (content, name) => {
   const obj = {
-    1: `【${content}に舞い降りた堕天使★${name}】`,
-    2: `【後に${content}の守護神となる者、それが${name}】`,
-    3: `【骨の髄まで${content}に忠実★${name}】`,
-    4: `【生粋の${content}チルドレン★${name}】`,
-    5: `【生まれ変わっても${content}に入りたい。${name}】`,
-    6: `【${content}のアルティメットフィクサー（究極の黒幕）${name}】`,
-    7: `【${content}の期待の星 ${name}】`,
-    8: `【${content}でもう一度青春がしたい。${name}】`,
-    9: `【三度の飯より${content}が好き。${name}】`
+    0: `【${content}に舞い降りた堕天使★${name}】`,
+    1: `【後に${content}の守護神となる者、それが${name}】`,
+    2: `【骨の髄まで${content}に忠実★${name}】`,
+    3: `【生粋の${content}チルドレン★${name}】`,
+    4: `【生まれ変わっても${content}に入りたい。${name}】`,
+    5: `【${content}のアルティメットフィクサー（究極の黒幕）${name}】`,
+    6: `【${content}の期待の星 ${name}】`,
+    7: `【${content}でもう一度青春がしたい。${name}】`,
+    8: `【三度の飯より${content}が好き。${name}】`
   }
 
   const num = Math.floor(Math.random() * 9)
@@ -19,15 +19,15 @@ const outputAffiliationCatchCopy = (content, name) => {
 }
 const outputSpecialSkill = (content, name) => {
   const obj = {
-    1: `【${content}ひとすじ20年・${name}】`,
-    2: `【奇跡の${content}使い・${name}名人】`,
-    3: `【${content}の鬼・その名も${name}】`,
-    4: `【息をするように${content}。${name}】`,
-    5: `【夢の中でも${content}したい・${name}】`,
-    6: `【${content}なら、${name}の右に出る者はいない】`,
-    7: `【寝ても醒めても${content}・${name}】`,
-    8: `【百戦錬磨の${content}術・${name}】`,
-    9: `【生まれ変わっても${content}したい・${name}】`
+    0: `【${content}ひとすじ20年・${name}】`,
+    1: `【奇跡の${content}使い・${name}名人】`,
+    2: `【${content}の鬼・その名も${name}】`,
+    3: `【息をするように${content}。${name}】`,
+    4: `【夢の中でも${content}したい・${name}】`,
+    5: `【${content}なら、${name}の右に出る者はいない】`,
+    6: `【寝ても醒めても${content}・${name}】`,
+    7: `【百戦錬磨の${content}術・${name}】`,
+    8: `【生まれ変わっても${content}したい・${name}】`
   }
 
   const num = Math.floor(Math.random() * 9)
